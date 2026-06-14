50c3b51a957742561c991cbb17bf5bc4639ce08a
Toffee35 <nailzagru@gmail.com>
2026-06-14 20:55:46 +0300

    update

--- home-manager/git-auth.nix
+++ home-manager/git-auth.nix
@@ -2,7 +2,18 @@
   programs.ssh = {
     enable = true;
     enableDefaultConfig = false;
-    matchBlocks."*".addKeysToAgent = "yes";
+    matchBlocks = {
+      "*".addKeysToAgent = "yes";
+      "github-main" = {
+        hostname = "github.com";
+        user = "git";
+        identityFile = "~/.ssh/id_ed25519";
+      };
+      "github-work" = {
+        hostname = "github.com";
+        user = "git";
+        identityFile = "~/.ssh/id_ed25519_work";
+      };
   };
 
   programs.git = {
